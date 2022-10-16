7c512d09e5e4bd0118b92853ce2f537edf002acf
Lawrence Macharia <macharialawrence2011@gmail.com>
2022-10-16 16:02:47 +0300

    starting the project with a lean template after stripping off unnecessary files

---
+++ src/services/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyAcTJicVh50W0rzRlVqkca2Dz1grjFc89k",
+    authDomain: "slack-clone-yt-8074d.firebaseapp.com",
+    projectId: "slack-clone-yt-8074d",
+    storageBucket: "slack-clone-yt-8074d.appspot.com",
+    messagingSenderId: "1090640484879",
+    appId: "1:1090640484879:web:18f2bb30291d6d5d78e17a"
+  };
