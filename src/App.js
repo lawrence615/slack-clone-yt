aaa1ebd6cfd1a34776ee7e07c650745fb984fb58
Lawrence Macharia <macharialawrence2011@gmail.com>
2022-10-17 16:29:17 +0300

    added routing

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import { createBrowserRouter, RouterProvider, Route } from "react-router-dom";
+
+import "./App.css";
+import Header from "./components/Header";
 
 function App() {
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <div>This is the Homepage</div>,
+    },
+  ]);
+
   return (
     <div className="app">
-      <h1>Let's build Slack</h1>
+      <Header/>
+      <React.StrictMode>
+        <RouterProvider router={router} />
+      </React.StrictMode>
     </div>
   );
 }
