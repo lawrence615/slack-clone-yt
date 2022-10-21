07e06563f09a520a1d57c2586ac0f240a56a10a2
Lawrence Macharia <macharialawrence2011@gmail.com>
2022-10-21 10:00:54 +0300

    Display of chat room name is possible and included ChatInput component

--- src/components/Chat.js
+++ src/components/Chat.js
@@ -1,16 +1,31 @@
 import React from "react";
+import { useSelector } from "react-redux";
 import styled from "styled-components";
 import StarBorderOutlinedIcon from "@mui/icons-material/StarBorderOutlined";
 import InfoOutlinedIcon from "@mui/icons-material/InfoOutlined";
+import { useDocument, useCollection } from "react-firebase-hooks/firestore";
+
+import ChatInput from "./ChatInput";
+import { selectRoomId } from "../features/appSlice";
+import { collection, db, doc, orderBy, query } from "../services/firebase";
 
 function Chat() {
+  const roomId = useSelector(selectRoomId);
+    // console.log(roomId)
+  const docRef = roomId && doc(db, "rooms", roomId);
+  const [roomDetails] = useDocument(roomId && docRef);
+  const [roomMessages] = useCollection(roomId && query(collection(docRef, "messages"), orderBy("timestamp", "asc")))
+
+  console.log('roomDetails',roomDetails?.data())
+//   console.log('roomMessages',roomMessages)
+
   return (
     <ChatContainer>
       <>
         <Header>
           <HeaderLeft>
             <h4>
-              <strong>#Room-name</strong>
+              <strong>#{roomDetails?.data().name}</strong>
             </h4>
             <StarBorderOutlinedIcon />
           </HeaderLeft>
@@ -20,6 +35,10 @@ function Chat() {
             </p>
           </HeaderRight>
         </Header>
+
+        <ChatMessages>{/** List out all messages */}</ChatMessages>
+
+        <ChatInput key={roomId} channelId={roomId} channelName={roomDetails?.data().name} />
       </>
     </ChatContainer>
   );
@@ -69,3 +88,5 @@ const HeaderRight = styled.div`
     font-size: 16px;
   }
 `;
+
+const ChatMessages = styled.div``;
