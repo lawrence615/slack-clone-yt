c3142563aa181defd5da54508b9320a89451652d
Lawrence Macharia <macharialawrence2011@gmail.com>
2022-10-23 15:03:34 +0300

    Showing loading animation when fetch details of logged in user

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import { createBrowserRouter, RouterProvider, Route } from "react-router-dom";
 import styled from "styled-components";
 import { useAuthState } from "react-firebase-hooks/auth";
+import Spinner from "react-spinkit";
 
 import Header from "./components/Header";
 import Sidebar from "./components/Sidebar";
@@ -9,6 +10,7 @@ import Chat from "./components/Chat";
 import { auth } from "./services/firebase";
 import Login from "./components/Login";
 import "./App.css";
+
 function App() {
   const [user, loading] = useAuthState(auth);
 
@@ -18,6 +20,20 @@ function App() {
       element: <Chat />,
     },
   ]);
+
+  if (loading) {
+    return (
+      <AppLoading>
+        <AppLoadingContents>
+          <img
+            src="https://cdn.mos.cms.futurecdn.net/SDDw7CnuoUGax6x9mTo7dd.jpg"
+            alt=""
+          />
+          <Spinner name="ball-spin-fade-loader" color="purple" fadeIn="none" />
+        </AppLoadingContents>
+      </AppLoading>
+    );
+  }
 
   return (
     <div className="app">
@@ -40,6 +56,28 @@ function App() {
 }
 
 export default App;
+
+const AppLoading = styled.div`
+  display: grid;
+  place-items: center;
+  height: 100vh;
+  width: 100%;
+`;
+
+const AppLoadingContents = styled.div`
+  text-align: center;
+  padding-bottom: 100px;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+
+  > img {
+    height: 100px;
+    padding: 20px;
+    margin-bottom: 40px;
+  }
+`;
 
 const AppBody = styled.div`
   display: flex;
