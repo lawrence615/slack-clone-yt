04f743b1a08f21fec0c7df9371fd46b572bb3b14
Lawrence Macharia <macharialawrence2011@gmail.com>
2022-10-23 13:25:01 +0300

    If user is not logged in, the only thing visible is the Login page

--- src/App.js
+++ src/App.js
@@ -1,32 +1,41 @@
 import React from "react";
 import { createBrowserRouter, RouterProvider, Route } from "react-router-dom";
 import styled from "styled-components";
+import { useAuthState } from "react-firebase-hooks/auth";
 
 import Header from "./components/Header";
 import Sidebar from "./components/Sidebar";
-import "./App.css";
 import Chat from "./components/Chat";
+import { auth } from "./services/firebase";
+import "./App.css";
+import Login from "./components/Login";
 
 function App() {
+  const [user, loading] = useAuthState(auth);
+
   const router = createBrowserRouter([
     {
       path: "/",
-      element: <Chat/>,
+      element: <Chat />,
     },
   ]);
 
   return (
     <div className="app">
-      <>
-        <Header />
-        <AppBody>
-          <Sidebar />
-          <React.StrictMode>
-            <RouterProvider router={router} />
-            {/** Chat */}
-          </React.StrictMode>
-        </AppBody>
-      </>
+      {!user ? (
+        <Login />
+      ) : (
+        <>
+          <Header />
+          <AppBody>
+            <Sidebar />
+            <React.StrictMode>
+              <RouterProvider router={router} />
+              {/** Chat */}
+            </React.StrictMode>
+          </AppBody>
+        </>
+      )}
     </div>
   );
 }
